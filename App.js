291d6098d761e33f1ce276e1d7c2e4ff27372527
Somil Singh <147026401+somilsingh8653@users.noreply.github.com>
2025-08-02 10:42:41 +0530

    getData

--- App.js
+++ App.js
@@ -7,6 +7,41 @@ const User = require("./src/models/user");
 
 app.use(express.json());
 
+
+//feed Api
+
+app.get("/feed" , async(req,res)=>{
+
+    try{
+     const user = await User.find({});
+     res.send(user);
+    }catch(err){
+     res.status(400).send("Something went wrong");
+    }
+});
+
+
+//get the data
+app.get("/user", async(req , res)=>{
+
+    const userEmail = req.body.emailId;
+
+    try{
+        
+        const users = await User.find({emailId:userEmail});
+        if(users.length === 0){
+            res.status(400).send("user not found")
+        }else{
+            res.send(users);
+        }
+    }catch(err){
+         res.status(400).send("Error saving the User"+err.message);
+     }
+})
+
+
+//Post the data into database
+
 app.post("/user", async(req,res)=>{
 
     const user = new User(req.body);
